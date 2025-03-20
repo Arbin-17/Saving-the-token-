98a24e0066ca298fb56589dbb2fe35229c693520
Arbin-17 <arbinkumar015@gmail.com>
2025-03-20 10:04:59 +0530

    updated

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
+const JWT_SECRET = "hvgbBhnsjaabkjaakjanaN";
 const encrypt = (payload) => {
   // encrypt the payload and return token
-}
+  const encryptedtoken = jwt.sign(
+    payload,
+    JWT_SECRET,
+    { expiresIn: "1h" },
+    { algorithm: "HS256" }
+  );
+  return encryptedtoken;
+};
 
 const decrypt = (token) => {
   // return decoded payload
-}
+  const decodedtoken = jwt.verify(token, JWT_SECRET);
+  // console.log(decodedtoken);
+  return decodedtoken;
+};
 
 module.exports = {
   encrypt,
-  decrypt
-}
+  decrypt,
+};
